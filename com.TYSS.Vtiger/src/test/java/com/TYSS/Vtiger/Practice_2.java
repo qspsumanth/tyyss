79d8f7dc2ed1cc015b857d8e0762980d0130ed60
HP <HP@DESKTOP-KP0OG9U>
2022-07-20 17:21:17 +0530

    commit1

---
+++ com.TYSS.Vtiger/src/test/java/com/TYSS/Vtiger/Practice_2.java
@@ -0,0 +1,19 @@
+package com.TYSS.Vtiger;
+
+import org.testng.annotations.Test;
+
+public class Practice_2 {
+
+		@Test
+		public void test() {
+			String url = System.getProperty("url");
+			String browser = System.getProperty("Browser");
+			//String username = System.getProperty("UserName");
+			//String password = System.getProperty("Password");
+		    System.out.println(url);
+			System.out.println(browser);
+			//System.out.println(username);
+		
+	}
+
+}
