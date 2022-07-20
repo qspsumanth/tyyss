06c238c08696f1d932ef4aade3c621099a365344
HP <HP@DESKTOP-KP0OG9U>
2022-07-20 16:24:35 +0530

    commit

--- com.TYSS.Vtiger/src/test/java/com/TYSS/Vtiger/Practice_01.java
+++ com.TYSS.Vtiger/src/test/java/com/TYSS/Vtiger/Practice_01.java
@@ -7,11 +7,11 @@ public class Practice_01 {
 
 		@Test
 		public void test() {
-			String url = System.getProperty("url");
+			//String url = System.getProperty("url");
 			String browser = System.getProperty("Browser");
 			//String username = System.getProperty("UserName");
 			//String password = System.getProperty("Password");
-			System.out.println(url);
+			//System.out.println(url);
 			System.out.println(browser);
 			//System.out.println(username);
 			
