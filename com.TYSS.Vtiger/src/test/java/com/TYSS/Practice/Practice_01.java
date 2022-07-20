58aa1c473c3f8b5600487010b1bf25fc489844d6
HP <HP@DESKTOP-KP0OG9U>
2022-07-20 14:15:03 +0530

    com.crm

---
+++ com.TYSS.Vtiger/src/test/java/com/TYSS/Practice/Practice_01.java
@@ -0,0 +1,50 @@
+package com.TYSS.Practice;
+
+import org.testng.annotations.Test;
+
+public class Practice_01 {
+	
+
+		@Test
+		public void test() {
+			String url = System.getProperty("url");
+			String browser = System.getProperty("Browser");
+			String username = System.getProperty("UserName");
+			String password = System.getProperty("Password");
+			System.out.println(url);
+			System.out.println(browser);
+			System.out.println(username);
+			
+			int a=10;
+			int b=20;
+			int res=a+b;
+			System.out.println(res);
+		}
+		@Test
+		public void test1() {
+			int a=10;
+			int b=20;
+			int res=a*b;
+			System.out.println(res);
+		}
+		@Test
+		public void test2() {
+			int a=100;
+			int b=10;
+			int c=a/b;
+			System.out.println(c);
+		}
+		@Test
+		public void test3()
+		{
+			System.out.println("hello1");
+			System.out.println("BiiiiiiiHiiiiiiii");
+			System.out.println("hello");
+			
+		}
+		
+
+	}
+
+
+
